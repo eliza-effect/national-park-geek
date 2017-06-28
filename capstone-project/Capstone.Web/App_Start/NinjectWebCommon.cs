f30074fae9ca8d6148165c2f5cb410ab3b2a58b1
Kevin Somerville <ksomerville0101@gmail.com>
2017-06-28 10:24:28 -0400

    setup and get all parks

--- capstone-project/Capstone.Web/App_Start/NinjectWebCommon.cs
+++ capstone-project/Capstone.Web/App_Start/NinjectWebCommon.cs
@@ -10,6 +10,8 @@ namespace Capstone.Web.App_Start
 
     using Ninject;
     using Ninject.Web.Common;
+    using Capstone.Web.DAL;
+    using System.Configuration;
 
     public static class NinjectWebCommon 
     {
@@ -61,6 +63,8 @@ namespace Capstone.Web.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-        }        
+            kernel.Bind<IParkDAL>().To<ParkSQLDAL>().WithConstructorArgument("connectionString", ConfigurationManager.ConnectionStrings["NPGeek"].ConnectionString);
+
+        }
     }
 }
---
+++ capstone-project/Capstone.Web/DAL/ParkSQLDAL.cs
@@ -0,0 +1,50 @@
+﻿using Capstone.Web.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.DAL
+{
+    public class ParkSQLDAL: IParkDAL
+    {
+        private string connectionString;
+        private const string SQL_GetAllParks = "Select * from park;";
+
+        public ParkSQLDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<Park> GetAllParks()
+        {
+            List<Park> allParks = new List<Park>();
+
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    allParks = conn.Query<Park>(SQL_GetAllParks).ToList();
+                }
+
+                return allParks;
+            }
+
+            catch(SqlException ex)
+            {
+                throw;
+            }
+        }
+
+        public Park GetPark(string parkID)
+        {
+            Park p = new Park();
+            return p;
+        }
+
+    }
+}
